df06e82f6755939ba90ee0c4e8fd284d4b83dd1c
julioseq <juliocesara8881@gmail.com>
2017-08-01 08:33:03 -0500

    prueba

---
+++ src/main/java/EjemploFor.java
@@ -0,0 +1,10 @@
+/**
+ * Created by HSEQ on 01/08/2017.
+ */
+public class EjemploFor {
+    public static void main(String[]args){
+        for (int i=0;i<100;i++){
+            System.out.println(i);
+        }
+    }
+}
